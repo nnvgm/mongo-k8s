0ef783f1f2fdbdedbd019df236513664e8210f5c
nnvgm <63500117+nnvgm@users.noreply.github.com>
2020-04-26 20:15:30 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo "Deploy Mongo To Kubernetes Namespace ${KUBE_NAMESPACE}"
-helm upgrade --install node-api . --namespace ${KUBE_NAMESPACE} --wait --atomic
+helm upgrade --install mongodb . --namespace ${KUBE_NAMESPACE} --wait --atomic
